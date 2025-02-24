9fb507a6685f7de6ac0b9fabe8f0e304c25364fd
Steven Gangnant <stevengangnant@hotmail.com>
2025-02-24 16:47:26 +0100

    Import

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const csv = require('csv-parser')
 const dateFns = require('date-fns');
 const cliTable = require('cli-table');
 
-const { analyticsFields } = require('./constants');
+const { analyticsFields, analyticsFieldBoolean } = require('./constants');
 const { getAnalytics, saveAnalyticsCsv } = require('./analytics');
 const { reformatDateFields } = require('./utils');
 
